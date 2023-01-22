d2ff067469acfc6a3c874c4aa95d23dab20d790c
devanzir <123272630+devanzir@users.noreply.github.com>
2023-01-22 10:59:44 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,19 @@
+
+const botaoTrailer = document.querySelector(".botao-trailer");
+const botaoFecharModal = document.querySelector(".fechar-modal");
+const video = document.getElementById("video");
+const modal = document.querySelector(".modal");
+const linkDoVideo = video.src;
+
+botaoTrailer.addEventListener("click", () => {
+ 
+  modal.classList.add("aberto");
+  video.setAttribute("src",linkDoVideo);
+});
+
+botaoFecharModal.addEventListener("click", () => {
+    modal.classList.remove("aberto");
+    video.setAttribute("src", "");
+});
+
+
